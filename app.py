cee2da71f3cda58786aaae837ee3f2dbe508231b
devops <devireddynaga1@gmail.com>
2025-04-14 14:56:15 -0400

    webhook

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from OpenShift!"
+    return "Hello from OpenShift! and welcome back for your first webhook"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
