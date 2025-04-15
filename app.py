3d8370c17dface1273a36ecc7699631639491f86
devops <devireddynaga1@gmail.com>
2025-04-15 10:22:35 -0400

    webhook tetsing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "welcome  naga  this is your first webhook"
+    return "webhook build triggred"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
